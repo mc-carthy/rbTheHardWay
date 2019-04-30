f3e8aac056cd9221605788eed0a1cfe079196067
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-30 21:09:12 +0100

    Excercise 1

---
+++ ex1/ex1.rb
@@ -0,0 +1,16 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+#Comment test
+
+=begin
+    multi
+    line
+    comment
+    test
+=end
